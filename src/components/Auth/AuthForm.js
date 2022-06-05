bd7dc709538aba4fb145a8c1a5ee4aaa753225e4
gergodancs <dancs.gergely@gmail.com>
2022-06-05 12:58:25 +0200

    implement sigup and signin logic firebase auth db

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -6,6 +6,7 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -16,37 +17,47 @@ const AuthForm = () => {
     console.log("submit");
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
-
+    setIsLoading(true);
+    let url;
     if (isLogin) {
-      console.log("szar");
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBAq0Sy1q8-DZWrTitzR9Z_OlfSdiGWUpQ";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBAq0Sy1q8-DZWrTitzR9Z_OlfSdiGWUpQ",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBAq0Sy1q8-DZWrTitzR9Z_OlfSdiGWUpQ";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
         if (res.ok) {
-          console.log(res, "ez az ok");
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed";
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
@@ -67,7 +78,12 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button type="submit">{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button type="submit">
+              {isLogin ? "Login" : "Create Account"}
+            </button>
+          )}
+          {isLoading && <p>Sending request..</p>}
           <button
             type="button"
             className={classes.toggle}
